4b897533b570dc8bf38363c8f688b0cef22cc534
cross <cross@TeamCity>
2017-08-03 09:52:55 +0000

    TeamCity change in 'my-test' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/MyTest/Project.kt
@@ -0,0 +1,25 @@
+package MyTest
+
+import jetbrains.buildServer.configs.kotlin.v10.*
+import jetbrains.buildServer.configs.kotlin.v10.Project
+import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.VersionedSettings
+import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.VersionedSettings.*
+import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.versionedSettings
+
+object Project : Project({
+    uuid = "0ea902b1-4cbc-4090-9198-0e9dc0f856bd"
+    extId = "MyTest"
+    parentId = "_Root"
+    name = "my-test"
+
+    features {
+        versionedSettings {
+            id = "PROJECT_EXT_1"
+            mode = VersionedSettings.Mode.ENABLED
+            buildSettingsMode = VersionedSettings.BuildSettingsMode.PREFER_CURRENT_SETTINGS
+            rootExtId = "MyTestRepo"
+            showChanges = true
+            settingsFormat = VersionedSettings.Format.KOTLIN
+        }
+    }
+})
